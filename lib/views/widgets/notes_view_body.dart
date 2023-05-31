ab45b01bbfe47b5630e7a29445b6f1bff4c6fd06
Osama Abo-Reda <osamaaboureida@gmail.com>
2023-05-31 05:35:59 +0300

    create cutom app bar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.0,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 12.0,
+          ),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
