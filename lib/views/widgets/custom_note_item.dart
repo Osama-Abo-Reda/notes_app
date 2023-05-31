e160d91f01dc806c7e41793e214852cc61aa76d9
Osama Abo-Reda <osamaaboureida@gmail.com>
2023-05-31 07:22:46 +0300

    create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //height: 120.0,
+      padding: const EdgeInsets.only(
+        top: 24.0,
+        bottom: 24.0,
+        left: 12.0,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(
+          16.0,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+                bottom: 16.0,
+              ),
+              child: Text(
+                'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                maxLines: 2,
+                style: TextStyle(
+                  color: Colors.black.withOpacity(
+                    0.5,
+                  ),
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 28.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24.0,
+            ),
+            child: Text(
+              'May 21, 2023',
+              style: TextStyle(
+                color: Colors.black.withOpacity(
+                  0.4,
+                ),
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
